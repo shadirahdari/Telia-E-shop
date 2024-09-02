2f4d4b9c88fe9f64c1e956cd5c934bdeebcc256f
Shadi <ra.rahdari@gmail.com>
2024-09-02 23:42:09 +0300

    started to add react forms

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import React from "react";
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +18,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
+  return   <React.StrictMode>
+     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  </React.StrictMode>
 }
+ 
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
-
+import Form from "./components/Form";
 import { ProductCard } from "./components/ProductCard";
-
 
 export default function Home() {
   return (
-   
     <div>
-       <ProductCard/>
-
+      <ProductCard />
+      <Form />
     </div>
-  
-
   );
 }
